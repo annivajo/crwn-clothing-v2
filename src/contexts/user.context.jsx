1bbfe84cd2219dde4aabba7036441f6d51269ca7
Anna Ivanova <anna@aitera.ca>
2022-07-06 12:47:24 -0400

    Add cart reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,6 @@
 import {createContext, useState, useEffect, useReducer} from "react";
 import {createUserDocumentFromAuth, onAuthStateChangedLister, signOutUser} from "../utils/firebase.utils";
+import {createAction} from "../utils/reducer/reducer.utils";
 
 export const UserContext = createContext({
     currentUser : null,
@@ -35,14 +36,13 @@ export const UserProvider = ({children}) => {
     const {currentUser} = state;
 
     const setCurrentUser = (user) => {
-        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER,user));
     }
 
 
     const value = {currentUser, setCurrentUser};
 
     useEffect(()=>{
-        console.log("USE EFFECT USER PROVIDER");
         const unsubscribe = onAuthStateChangedLister((user)=>{
             if(user){
                 createUserDocumentFromAuth(user);
